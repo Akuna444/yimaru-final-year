fc679f0a7de0cd7aef9a00371a0de0730f81dddb
Akuna444 <aklilsolomon3@gmail.com>
2024-05-03 14:30:34 +0300

    add: locale to trpc

--- app/[locale]/api/trpc/[trpc]/route.ts
+++ app/[locale]/api/trpc/[trpc]/route.ts
@@ -1,13 +1,14 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/trpc";
-console.log("ammmda running");
+import { getLocale } from "next-intl/server";
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
-    endpoint: "/en/api/trpc",
+const handler = async (req: Request) => {
+  const locale = await getLocale();
+  return fetchRequestHandler({
+    endpoint: `/${locale}/api/trpc`,
     req,
     router: appRouter,
     createContext: () => ({}),
   });
-
+};
 export { handler as GET, handler as POST };
