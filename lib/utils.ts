d86e89b1f6bfb3080609ec673f8b7840fb1e8f9d
Akuna444 <aklilsolomon3@gmail.com>
2024-05-03 20:32:13 +0300

    add: quiz util files

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,3 +11,20 @@ export function absoluteUrl(path: string) {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
+
+export function formatTimeDelta(seconds: number) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds - hours * 3600) / 60);
+  const secs = Math.floor(seconds - hours * 3600 - minutes * 60);
+  const parts = [];
+  if (hours > 0) {
+    parts.push(`${hours}h`);
+  }
+  if (minutes > 0) {
+    parts.push(`${minutes}m`);
+  }
+  if (secs > 0) {
+    parts.push(`${secs}s`);
+  }
+  return parts.join(" ");
+}
