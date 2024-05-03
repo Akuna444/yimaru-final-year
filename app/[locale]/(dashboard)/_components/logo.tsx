ab063885a21816dc35a6f148dbb1b4cd66c6ceaa
Akuna444 <aklilsolomon3@gmail.com>
2024-05-03 14:29:29 +0300

    add link to logo

--- app/[locale]/(dashboard)/_components/logo.tsx
+++ app/[locale]/(dashboard)/_components/logo.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image height={200} width={200} alt="logo" src="/Yimaru_transparent.png" />
+    <Link href="/">
+      <Image
+        className="w-[200px] h-fit"
+        height={200}
+        width={200}
+        alt="logo"
+        src="/Yimaru_transparent.png"
+      />
+    </Link>
   );
 };
