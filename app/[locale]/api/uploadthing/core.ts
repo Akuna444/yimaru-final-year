efe5baf421531a7f94dc471e47977be30a268430
Akuna444 <aklilsolomon3@gmail.com>
2024-05-03 14:30:05 +0300

    config: course attachment upload

--- app/[locale]/api/uploadthing/core.ts
+++ app/[locale]/api/uploadthing/core.ts
@@ -18,7 +18,7 @@ export const ourFileRouter = {
   courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(middleware)
     .onUploadComplete(() => {}),
-  courseAttachment: f(["pdf"])
+  courseAttachment: f({ pdf: { maxFileCount: 1, maxFileSize: "16MB" } })
     .middleware(middleware)
     .onUploadComplete(() => {}),
   chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
