e864701f94bb42500569871163bc03bcefaf5399
Akuna444 <aklilsolomon3@gmail.com>
2024-05-04 10:26:15 +0300

    fix: quiz history route

--- app/[locale]/(dashboard)/_components/navbar.tsx
+++ app/[locale]/(dashboard)/_components/navbar.tsx
@@ -7,12 +7,13 @@ import { TimerIcon } from "lucide-react";
 export const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      {" "}
+      <MobileSidebar />{" "}
       <Link href="/quiz/history">
         <div className="flex">
           <TimerIcon /> <span>Quiz History</span>
         </div>
-      </Link>{" "}
-      <MobileSidebar />
+      </Link>
       <NavbarRoutes />
       <LanguageSwitcher />
     </div>
